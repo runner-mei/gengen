711cd0ee16e39207049061f0a71275a6957fd935
runner.mei <runner.mei@gmail.com>
2016-02-17 16:00:08 +0800

    add column definitions

--- base.go
+++ base.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
 
 	"github.com/Masterminds/squirrel"
+	"github.com/lann/builder"
 )
 
 var ErrNotUpdated = errors.New("no record is updated")
 var ErrNotDeleted = errors.New("no record is deleted")
 
+func isPostgersql(db interface{}) bool {
+	return true
+}
+
 func isPlaceholderWithDollar(value interface{}) bool {
 	return true
 }
 
-type DbModel struct {
+type ViewModel struct {
 	TableName   string
 	ColumnNames []string
-	KeyNames    []string
 }
 
-func (self *DbModel) UpdateBy(db squirrel.BaseRunner, values map[string]interface{}, pred interface{}, args ...interface{}) (int64, error) {
+func (self *ViewModel) Where(exprs ...Expr) squirrel.StatementBuilderType {
+	sqlizers := make([]squirrel.Sqlizer, 0, len(exprs))
+	for _, exp := range exprs {
+		sqlizers = append(sqlizers, exp)
+	}
+
+	pred, args, _ := squirrel.And(sqlizers).ToSql()
+	return squirrel.StatementBuilderType(builder.Append(squirrel.StatementBuilder, "WhereParts", pred, args).(builder.Builder))
+}
+
+func (self *ViewModel) UpdateBy(db squirrel.BaseRunner, values map[string]interface{}, pred interface{}, args ...interface{}) (int64, error) {
 	sql := squirrel.Update(self.TableName)
 	if isPlaceholderWithDollar(db) {
 		sql = sql.PlaceholderFormat(squirrel.Dollar)
@@ -38,7 +53,20 @@ func (self *DbModel) UpdateBy(db squirrel.BaseRunner, values map[string]interfac
 	return result.RowsAffected()
 }
 
-func (self *DbModel) UpdateByKey(db squirrel.BaseRunner, values map[string]interface{}, keys ...interface{}) error {
+func (self *ViewModel) DeleteBy(db squirrel.BaseRunner, pred interface{}, args ...interface{}) (int64, error) {
+	result, e := squirrel.Delete(self.TableName).Where(pred, args).RunWith(db).Exec()
+	if nil != e {
+		return 0, e
+	}
+	return result.RowsAffected()
+}
+
+type DbModel struct {
+	ViewModel
+	KeyNames []string
+}
+
+func (self *DbModel) UpdateByPrimaryKey(db squirrel.BaseRunner, values map[string]interface{}, keys ...interface{}) error {
 	sql := squirrel.Update(self.TableName)
 	if isPlaceholderWithDollar(db) {
 		sql = sql.PlaceholderFormat(squirrel.Dollar)
@@ -70,15 +98,7 @@ func (self *DbModel) UpdateByKey(db squirrel.BaseRunner, values map[string]inter
 	return nil
 }
 
-func (self *DbModel) DeleteBy(db squirrel.BaseRunner, pred interface{}, args ...interface{}) (int64, error) {
-	result, e := squirrel.Delete(self.TableName).Where(pred, args).RunWith(db).Exec()
-	if nil != e {
-		return 0, e
-	}
-	return result.RowsAffected()
-}
-
-func (self *DbModel) DeleteByKey(db squirrel.BaseRunner, keys ...interface{}) error {
+func (self *DbModel) DeleteByPrimaryKey(db squirrel.BaseRunner, keys ...interface{}) error {
 	sql := squirrel.Delete(self.TableName)
 	if isPlaceholderWithDollar(db) {
 		sql = sql.PlaceholderFormat(squirrel.Dollar)
@@ -105,4 +125,85 @@ func (self *DbModel) DeleteByKey(db squirrel.BaseRunner, keys ...interface{}) er
 
 type ColumnModel struct {
 	Name string
+}
+
+func (self *ColumnModel) EQU(value interface{}) Expr {
+	return Expr{Column: self, Operator: "=", Value: value}
+}
+
+func (self *ColumnModel) NEQ(value interface{}) Expr {
+	return Expr{Column: self, Operator: "<>", Value: value}
+}
+
+type Expr struct {
+	Column   *ColumnModel
+	Operator string
+	Value    interface{}
+}
+
+func (self Expr) ToSql() (string, []interface{}, error) {
+	return self.Column.Name + " " + self.Operator + " ? ", []interface{}{self.Value}, nil
+}
+
+// Sqlizer is the interface that wraps the ToSql method.
+//
+// ToSql returns a SQL representation of the Sqlizer, along with a slice of args
+// as passed to e.g. database/sql.Exec. It can also return an error.
+type Sqlizer interface {
+	ToSql() (string, []interface{}, error)
+}
+
+// Execer is the interface that wraps the Exec method.
+//
+// Exec executes the given query as implemented by database/sql.Exec.
+type Execer interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+// Queryer is the interface that wraps the Query method.
+//
+// Query executes the given query as implemented by database/sql.Query.
+type Queryer interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+}
+
+// QueryRower is the interface that wraps the QueryRow method.
+//
+// QueryRow executes the given query as implemented by database/sql.QueryRow.
+type QueryRower interface {
+	QueryRow(query string, args ...interface{}) RowScanner
+}
+
+// BaseRunner groups the Execer and Queryer interfaces.
+type BaseRunner interface {
+	Execer
+	Queryer
+}
+
+// Runner groups the Execer, Queryer, and QueryRower interfaces.
+type Runner interface {
+	Execer
+	Queryer
+	QueryRower
+}
+
+// RowScanner is the interface that wraps the Scan method.
+//
+// Scan behaves like database/sql.Row.Scan.
+type RowScanner interface {
+	Scan(...interface{}) error
+}
+
+// Row wraps database/sql.Row to let squirrel return new errors on Scan.
+type Row struct {
+	RowScanner
+	err error
+}
+
+// Scan returns Row.err or calls RowScanner.Scan.
+func (r *Row) Scan(dest ...interface{}) error {
+	if r.err != nil {
+		return r.err
+	}
+	return r.RowScanner.Scan(dest...)
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 
@@ -23,9 +24,49 @@ func (cmd *versionCommand) Run(args []string) {
 	fmt.Println(filepath.Base(os.Args[0]), "1.0")
 }
 
+type embedeCommand struct {
+	//file string
+}
+
+func (cmd *embedeCommand) Flags(fs *flag.FlagSet) *flag.FlagSet {
+	// define subcommand's flags
+	//fs.StringVar(&cmd.file, "file", "", "provides")
+	return fs
+}
+
+func (cmd *embedeCommand) Run(args []string) {
+	if len(args) != 2 {
+		fmt.Println("argument error.")
+		return
+	}
+	out, e := os.OpenFile(args[0], os.O_TRUNC|os.O_RDWR|os.O_CREATE, 0)
+	if nil != e {
+		fmt.Println(e)
+		return
+	}
+	defer out.Close()
+	in, e := os.OpenFile(args[1], os.O_RDONLY, 0)
+	if nil != e {
+		fmt.Println(e)
+		return
+	}
+	defer in.Close()
+
+	out.WriteString(`package main
+
+var embede_text = `)
+	out.WriteString("`")
+	if _, e = io.Copy(out, in); nil != e {
+		fmt.Println(e)
+		return
+	}
+	out.WriteString("`")
+}
+
 func init() {
 	// register version as a subcommand
 	command.On("version", "prints the version", &versionCommand{}, nil)
+	command.On("embede", "", &embedeCommand{}, nil)
 }
 
 func main() {
